26dd4236a9a00560d9fe50cf4548ca16e912aef1
samjingwen <samjingwen@u.nus.edu>
2026-08-05 21:22:28 +0800

    Add Quick Look previews to Mini Files

--- .config/nvim/lua/plugins/extend-mini-files.lua
+++ .config/nvim/lua/plugins/extend-mini-files.lua
@@ -1,5 +1,27 @@
 return {
   "nvim-mini/mini.files",
+  init = function()
+    local group = vim.api.nvim_create_augroup("mini_files_quick_look", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = group,
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        vim.keymap.set("n", "P", function()
+          local entry = require("mini.files").get_fs_entry()
+          if not entry or entry.fs_type ~= "file" then
+            vim.notify("Select a file to preview", vim.log.levels.WARN)
+            return
+          end
+
+          vim.system({ "qlmanage", "-p", entry.path }, {
+            detach = true,
+            stdout = false,
+            stderr = false,
+          })
+        end, { buffer = args.data.buf_id, desc = "Preview with Quick Look" })
+      end,
+    })
+  end,
   keys = {
     {
       "<leader>e",
